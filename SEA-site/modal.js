a8ff661c13d5ac4f8eb3da48e934f36ba6fe6dc9
Sanspairos_0mega <57668675+Marcelino-Morais@users.noreply.github.com>
2025-11-05 15:54:56 -0300

    Delete SEA-site directory

--- SEA-site/modal.js
+++
@@ -1,9 +0,0 @@
-function abrirJanela() {
-    document.getElementById('janela-modal').style.display = 'block';
-    document.getElementById('overlay').style.display = 'block';
-}
-
-function fecharJanela() {
-    document.getElementById('janela-modal').style.display = 'none';
-    document.getElementById('overlay').style.display = 'none';
-}
